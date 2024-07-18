e1efb921329d55c0d2dd7d8bf4e6ee8fe523e3a0
Matheusp007-226 <matheus.007brasil@gmail.com>
2024-07-17 21:20:41 -0300

    Retirado o script de inicialização de alguns dados do banco

--- desafiovotacaofullstack/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO associate (name, cpf) VALUES
-('Lucas Santos', '670.157.110-79'),
-('Manoel Dias', '837.762.740-00'),
-('Ótavio Pereira', '338.228.520-74'),
-('Roberto Cassandro', '032.490.460-60');
-
-INSERT INTO agenda (description) VALUES
-('Você faria um curso gratuito de programação?'),
-('Você possui um lado político?');
