0754fd2ca5fcd35ea65c9719f001913550c6d28e
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-07-20 01:36:17 -0300

    Models User, Role and the loginDTO AND CorsConfig

---
+++ backend/src/main/java/com/antonybresolin/backend/controller/dto/LoginRequest.java
@@ -0,0 +1,3 @@
+package com.antonybresolin.backend.controller.dto;
+
+public record LoginRequest (String cpf, String password) {}
---
+++ backend/src/main/java/com/antonybresolin/backend/entities/User.java
@@ -0,0 +1,65 @@
+package com.antonybresolin.backend.entities;
+
+import com.antonybresolin.backend.controller.dto.LoginRequest;
+import jakarta.persistence.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+
+    @Column(unique = true, length = 11)
+    private String cpf;
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isLoginCorrect(LoginRequest loginRequest, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(loginRequest.password(), this.password);
+    }
+}
