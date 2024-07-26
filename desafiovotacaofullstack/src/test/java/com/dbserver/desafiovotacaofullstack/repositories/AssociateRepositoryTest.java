2d761285278ecdd48370e588a1b12df63fb33b7c
Matheusp007-226 <matheus.007brasil@gmail.com>
2024-07-25 23:13:29 -0300

    feature(testes): Implementação de testes unitários no service e repository de associates.

---
+++ desafiovotacaofullstack/src/test/java/com/dbserver/desafiovotacaofullstack/repositories/AssociateRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.dbserver.desafiovotacaofullstack.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.dbserver.desafiovotacaofullstack.domains.Associate;
+import com.dbserver.desafiovotacaofullstack.dtos.AssociateRequestDto;
+
+import jakarta.persistence.EntityManager;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class AssociateRepositoryTest {
+	
+	@Autowired
+	EntityManager entityManager;
+	
+	@Autowired
+	AssociateRepository associateRepository;
+	
+	@Test
+	@DisplayName("Should if exists associate successfully from DB.")
+	public void existsByCpfSuccess() {
+		Associate associate = createAssociate(new AssociateRequestDto("Thales Bento Mário Rezende", "704.718.699-96"));
+		boolean isExistsAssociate = associateRepository.existsByCpf(associate.getCpf());
+		assertThat(isExistsAssociate).isTrue();
+	}
+	
+	@Test
+	@DisplayName("Should false if not exists associate from DB.")
+	public void existsByCpfErrors() {
+		boolean isExistsAssociate = associateRepository.existsByCpf("334.440.257-93");
+		assertThat(isExistsAssociate).isFalse();
+	}
+	
+	private Associate createAssociate(AssociateRequestDto associateRequestDto) {
+		Associate associate = new Associate(associateRequestDto.name(), associateRequestDto.cpf());
+		this.entityManager.persist(associate);
+		return  associate;
+	}
+}
